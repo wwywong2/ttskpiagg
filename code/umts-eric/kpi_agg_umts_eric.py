ede1125eeee6c094d2c3658f00f738cc66202ae1
Wesley Wong <32700085+wwywong2@users.noreply.github.com>
2017-10-23 18:48:26 -0700

    2017-05-06

--- code/umts-eric/kpi_agg_umts_eric.py
+++ code/umts-eric/kpi_agg_umts_eric.py
@@ -3,6 +3,7 @@
 ## Imports
 import sys
 import os
+import glob # pathname
 import time
 import uuid
 import json
@@ -35,19 +36,24 @@ curr_py_dir, curr_py_filename = os.path.split(curr_py_path)  # current file and 
 
 # argv[1] - process name
 # argv[2] - input file (csv)
-# argv[3] - output parquet dir
-# argv[4] - process mode: 'client' or 'cluster'
+# argv[3] - schema file (json) (empty if not needed)
+# argv[4] - output parquet dir
+# argv[5] - process mode: 'client' or 'cluster'
 
 APP_NAME = "kpiAggrApp"
 # argv[1] - take app name from param
 if len(sys.argv) > 1:
    APP_NAME = sys.argv[1]
 
-
-# argv[3] - output dir
-output_dir = ""
+# argv[3] - schema filename
+schemaFile = ""
 if len(sys.argv) > 3:
-   output_dir = sys.argv[3]
+   schemaFile = sys.argv[3]
+
+# argv[4] - output dir
+output_dir = ""
+if len(sys.argv) > 4:
+   output_dir = sys.argv[4]
 output_dir = output_dir.rstrip('/')
 #output_dir = curr_py_dir+'/output_'+time.strftime("%Y%m%d%H%M%S")
 
@@ -65,10 +71,10 @@ else:
 
 
 
-# argv[4] - process mode
+# argv[5] - process mode
 proc_mode = ''
-if len(sys.argv) > 4:
-   proc_mode = sys.argv[4]
+if len(sys.argv) > 5:
+   proc_mode = sys.argv[5]
 proc_mode = proc_mode.lower()
 if not proc_mode == 'cluster':
    proc_mode = 'client'
@@ -79,17 +85,59 @@ if not proc_mode == 'cluster':
 
 ##OTHER FUNCTIONS/CLASSES
 
+'''
+def f_map(filetuple):
+   [fn,bw] = filetuple
+
+   try:
+      xml = XMLParser(fn,'bytes','file',bw,SparkFiles.get('config.ini'))
+      #util.logMessage("inside map func, after XMLParser(), before xml.ProcessXML()")
+      return xml.ProcessXML()
+   except Exception as e:
+      util.logMessage("err in file: %s" % fn)
+      return ""
+
+def f_reduce(a, b):
+   #util.logMessage("inside reduce func")
+   return a + b
+'''
+
+
+def sampleCode(spark):
+
+   # read parquet
+   pqDF = spark.read.parquet('/mnt/nfs/test/eric_new3.pqz')
+   pqDF.show(30, truncate=False)
+
+   # create view for sql
+   pqDF.createOrReplaceTempView('eric')
+
+   # sql
+   sqlDF = spark.sql("SELECT distinct HL_MARKET FROM eric")
+   sqlDF.show()
+
+   # write to parquet, consolidate to 1 file (takes time)
+   sqlDF.coalesce(1).write.parquet('/mnt/nfs/test/eric_new4.pqz',
+          compression='gzip',
+          mode='overwrite',
+          partitionBy='newtime')
+
+   # drop a view
+   spark.catalog.dropTempView("eric")
+
+
+
 def csvToDF(spark, csvFile, schema=None):
 
    try:
       if schema is None:
-         df = spark.read.csv(filename,
+         df = spark.read.csv(csvFile,
                              ignoreLeadingWhiteSpace=True,
                              ignoreTrailingWhiteSpace=True,
                              header=True,
                              timestampFormat='yyyy-MM-dd HH:mm')
       else:
-         df = spark.read.csv(filename,
+         df = spark.read.csv(csvFile,
                              ignoreLeadingWhiteSpace=True,
                              ignoreTrailingWhiteSpace=True,
                              header=True,
@@ -129,25 +177,125 @@ def jsonToSchema(jsonFile):
    return schema
 
 
+# read each csv into df then export to parquet
+def csvToParquet(spark, inputCsv, schema, outputDir):
+
+   # read csv file(s) into dataframe
+   filecount = 0 # init
+
+   # check file exist
+   inputCsvList = glob.glob(inputCsv)
+   #if len(glob.glob(inputCsv)) <= 0:  # no file
+   if len(inputCsvList) <= 0:  # no file
+      util.logMessage("no file to process: %s" % inputCsv)
+      util.logMessage("Process terminated.")
+      return 0
+
+   for curr_file in sorted(inputCsvList):
+
+      filecount += 1
+      util.logMessage("reading file: %s" % curr_file)
+
+      df = csvToDF(spark, curr_file, schema)
+      if df is None:
+         util.logMessage("issue(s) reading file: %s" % curr_file)
+      else:
+
+         util.logMessage("finish reading file: %s" % curr_file)
+
+         # write parquet
+         if filecount == 1:
+            writemode = 'overwrite'
+         else:
+            writemode = 'append'
+         addPkAndSaveParquet(df, writemode, outputDir)
+
+   return 0
 
 
 
-'''
-def f_map(filetuple):
-   [fn,bw] = filetuple
 
-   try:
-      xml = XMLParser(fn,'bytes','file',bw,SparkFiles.get('config.ini'))
-      #util.logMessage("inside map func, after XMLParser(), before xml.ProcessXML()")
-      return xml.ProcessXML()
-   except Exception as e:
-      util.logMessage("err in file: %s" % fn)
-      return ""
+# read group of csvs and union into df then export to parquet
+def csvToParquet2(spark, inputCsv, schema, outputDir, loadFactor=10):
 
-def f_reduce(a, b):
-   #util.logMessage("inside reduce func")
-   return a + b
-'''
+   # read csv file(s) into dataframe
+   filecount = 0 # init
+   firsttime = True
+
+   # check file exist
+   inputCsvList = glob.glob(inputCsv)
+   #if len(glob.glob(inputCsv)) <= 0:  # no file
+   if len(inputCsvList) <= 0:  # no file
+      util.logMessage("no file to process: %s" % inputCsv)
+      util.logMessage("Process terminated.")
+      return 0
+
+   for curr_file in sorted(inputCsvList):
+
+      util.logMessage("reading file: %s" % curr_file)
+
+      df = csvToDF(spark, curr_file, schema)
+      if df is None:
+         util.logMessage("issue(s) reading file: %s" % curr_file)
+      else:
+         util.logMessage("finish reading file: %s" % curr_file)
+
+      if filecount % loadFactor == 0:
+         if filecount is not 0: # not the first time, write/append to file
+
+            # write parquet
+            if firsttime:
+               writemode = 'overwrite'
+               firsttime = False
+            else:
+               writemode = 'append'
+            addPkAndSaveParquet(maindf, writemode, outputDir)
+
+         maindf = df
+      else:
+         maindf = maindf.union(df)
+
+      filecount += 1
+      # end of for curr_file in sorted(inputCsvList):
+
+
+   if firsttime:
+      writemode = 'overwrite'
+      firsttime = False
+   else:
+      writemode = 'append'
+   addPkAndSaveParquet(maindf, writemode, outputDir)
+
+
+   return 0
+
+
+
+def addPkAndSaveParquet(df, writemode, outputDir):
+
+   util.logMessage("adding partition columns...")
+   # add key col from HL_MARKET - need to add HL_MARKET because that column will be gone if we go into sub dir
+   df = df.withColumn("pk_market", df['HL_MARKET'])
+   # add key col from HL_DATE
+   df = df.withColumn("pk_date", date_format(df['HL_DATE'], 'yyyy-MM-dd'))
+   # add key col from PERIOD_START_TIME
+   df = df.withColumn("pk_hr", date_format(df['PERIOD_START_TIME'], 'HH'))
+
+   # show dtypes
+   #util.logMessage("dtypes: %s" % df.dtypes)
+   # show schema
+   #df.printSchema()
+   #df.show(1,truncate=False)
+
+   util.logMessage("start writing parquet file (%s): %s" % (writemode, outputDir))
+   df.write.parquet(outputDir,
+                    compression='gzip',
+                    mode=writemode,
+                    partitionBy=('pk_market','pk_date','pk_hr'))
+   util.logMessage("finish writing parquet file (%s): %s" % (writemode, outputDir))     
+
+
+
 
 
 
@@ -161,9 +309,11 @@ def main(spark,filename,outfilename):
       sc = spark.sparkContext
 
       if proc_mode == 'client':
-         # add file
-         util.logMessage("addFile: %s" % curr_py_dir+'/umts_eric_schema.json')
-         sc.addFile(curr_py_dir+'/umts_eric_schema.json')
+
+         # add file        
+         if schemaFile is not "":
+            util.logMessage("addFile: %s" % curr_py_dir+'/'+schemaFile)
+            sc.addFile(curr_py_dir+'/'+schemaFile)
 
          # add py reference
          util.logMessage("addPyFile: %s" % curr_py_dir+'/util.py')
@@ -176,47 +326,31 @@ def main(spark,filename,outfilename):
 
 
       # get schema
-      schemaFile = "umts_eric_schema.json"
-      schema = jsonToSchema(schemaFile)
-      util.logMessage("acquired schema from file: %s" % schemaFile)
+      schema = None # init
+      #schemaFile = "umts_eric_schema.json"
+      if schemaFile is not "":
+         #schema = jsonToSchema(SparkFiles.get(schemaFile)) # might need this one when running cluster mode
+         schema = jsonToSchema(curr_py_dir+'/'+schemaFile)
+
+      if schema is not None:
+         util.logMessage("acquired schema from file: %s" % schemaFile)
+      else:
+         util.logMessage("assumed no schema")
 
 
-      # read csv file into dataframe
-      util.logMessage("reading file: %s" % filename)
-      df = csvToDF(spark, filename, schema)
-      if df is None:
-         util.logMessage("issue(s) reading file: %s" % filename)
-      else:         
-         util.logMessage("finish reading file: %s" % filename)
+      # read csv file(s) into dataframe then save into parquet
+      #csvToParquet(spark, filename, schema, output_dir) # old way - read 1 save 1
+      csvToParquet2(spark, filename, schema, output_dir, 20) # new way - read 20 save 1
 
-         # show dtypes
-         #util.logMessage("dtypes: %s" % df.dtypes)
-         # show schema
-         #df.printSchema()
+      # sample code
+      #sampleCode(spark)
 
-         pqf = spark.read.parquet('/mnt/nfs/test/eric_new3.pqz')
-         pqf.show(30, truncate=False)
-         pqf.createOrReplaceTempView('eric')
-         sqlDF = spark.sql("SELECT distinct HL_MARKET FROM eric")
-         #sqlDF.coalesce(1).write.parquet('/mnt/nfs/test/eric_new4.pqz', compression='gzip', mode='overwrite')
-         sqlDF.show()
-
-         time.sleep(120)
+      # here need to do aggregation by hour and save to another pq
 
 
 
 
-
-      # stop spark session (and context)
-      spark.stop()
-
-
-      util.logMessage("finish process successfully.")
-
-      return 0
-
-
-
+      #time.sleep(120)
 
 
    except Exception as e:
@@ -235,122 +369,16 @@ def main(spark,filename,outfilename):
 
 
 
-   '''
-   curr_try_num = 0
-   bSocketConnFail = True # init to True so it will go into loop
-   while (curr_try_num < socket_retry_num and bSocketConnFail):
-
-      try:
-
-         # map
-         #util.logMessage("starting map")
-         mapRDD = textRDD.map(f_map)
-         #util.logMessage("after map, starting reduce")
-
-         #print mapRDD.count()
-         #print mapRDD.collect()
-
-
-         # reduce
-         redRDD = mapRDD.reduce(f_reduce)
-         #util.logMessage("after reduce")
-
-         #print redRDD.count()
-         #print redRDD.collect()
-
-
-      except socket.error as e:
-
-         util.logMessage("Job: %s: Socket Error: %s!" % (APP_NAME, e))
-         curr_try_num += 1 # increment retry count
-         if curr_try_num < socket_retry_num:
-            util.logMessage("Job: %s: will retry in %d sec" % (APP_NAME, socket_retry_sec))
-            time.sleep(socket_retry_sec)
-         else:
-            util.logMessage("Job: %s: too many retry (%d)! Give up!" % (APP_NAME, socket_retry_num))
-
-      except socket.timeout as e:
-
-         util.logMessage("Job: %s: Socket Timeout: %s!" % (APP_NAME, e))
-         curr_try_num += 1 # increment retry count
-         if curr_try_num < socket_retry_num:
-            util.logMessage("Job: %s: will retry in %d sec" % (APP_NAME, socket_retry_sec))
-            time.sleep(socket_retry_sec)
-         else:
-            util.logMessage("Job: %s: too many retry (%d)! Give up!" % (APP_NAME, socket_retry_num))
-
-      except Exception as e: # trying to catch could not open socket
-
-         if hasattr(e, 'message') and e.message == 'could not open socket':
-
-            util.logMessage("Job: %s: Socket Error: %s!" % (APP_NAME, e))
-            curr_try_num += 1 # increment retry count
-            if curr_try_num < socket_retry_num:
-               util.logMessage("Job: %s: will retry in %d sec" % (APP_NAME, socket_retry_sec))
-               time.sleep(socket_retry_sec)
-            else:
-               util.logMessage("Job: %s: too many retry (%d)! Give up!" % (APP_NAME, socket_retry_num))
-
-         else:
-
-            util.logMessage('Cleanup location \'%s\'' % output_dir)
-            os.system("rm -rf \'%s\'" % output_dir) 
-            util.logMessage("Job: %s: Other Exception Error: %s!" % (APP_NAME, e))
-            raise # not the error we are looking for
-
-      except:
-
-         util.logMessage('Cleanup location \'%s\'' % output_dir)
-         os.system("rm -rf \'%s\'" % output_dir) 
-         util.logMessage("Job: %s: Other Unknown Error!" % APP_NAME)
-         raise # not the error we are looking for
-
-      else:
-
-         bSocketConnFail = False
-
-
-
-
-   if bSocketConnFail: # max retry reached, still fail
-      util.logMessage("socket issue(s), failed parsing job: %s" % APP_NAME)
-      util.logMessage('Cleanup location \'%s\'' % output_dir)
-      os.system("rm -rf \'%s\'" % output_dir) 
-      return 1
-   '''
-
-
-
-   # print to file
-   input_dir, input_filename = os.path.split(filename + '.txt')  # current file and folder - abs path
-   input_dir, output_gz_file = os.path.split(filename + '_' + time.strftime("%Y%m%d%H%M%S") + '.tgz')
-   output_filename = output_dir + '/' + input_filename
-   err_filename = output_filename.split('.txt')[0] + '.error.txt'
-
-   util.logMessage("start writing file: %s" % output_filename)
-
-   err = ""
-   rslt = ""
-   with open(output_filename,'w') as fout:
-      uuidstr = str(uuid.uuid4())
-      mycache = list()
-      header = XMLParser.GetHeader(curr_py_dir+'/config.ini')
-      fout.write(header+'\n')
-      [rslt, err] = XMLParser.GetReport(redRDD, mycache)
-      for row in rslt.split("\n"):
-         if len(row) > 1:
-            fout.write(uuidstr+','+row+'\n')
-
-   # output error log
-   with open(err_filename,'w') as ferr:
-      ferr.write(err)
-
-
-   util.logMessage("finish writing file: %s" % output_filename)
-
 
    # stop spark session (and context)
    spark.stop()
+
+   util.logMessage("finish process successfully.")
+   return 0
+
+
+
+   # WES_TEST: review below whether it is needed
 
    try:
 
@@ -426,7 +454,7 @@ if __name__ == "__main__":
       sys.exit(2)
 
    filename = sys.argv[2]
-   outfilename = sys.argv[3]
+   outfilename = sys.argv[4]
 
    # Configure Spark
    spark = SparkSession \
